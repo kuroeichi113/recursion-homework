df7a188d2574a71ff4580fdc79213a34e0bc13a5
stas_tarasevich <kuroe-ichi@yandex.ru>
2024-01-26 18:59:21 +0300

    Третья задача

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 5, 8, 7, 4, 9 };
+PrintArrayReverse(array, array.Length - 1);
+
+
+void PrintArrayReverse(int[] array, int index)
+{
+    if (index >= 0)
+    {
+        Console.Write($"{array[index]} ");
+        PrintArrayReverse(array, index - 1);
+    }
+}
+
+
