83d485a9820f66033345727aafcdf3b86e6a4c06
stas_tarasevich <kuroe-ichi@yandex.ru>
2024-01-26 18:33:32 +0300

    Первая задача

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите значения M и N:");
+int M = (Console.ReadLine());
+int N = (Console.ReadLine());
+
+PrintNaturalNumbers(M, N);
+
+static void PrintNaturalNumbers(int M, int N)
+{
+    if (M <= N)
+    {
+        Console.Write($"{M} ");
+        PrintNaturalNumbers(M + 1, N);
+    }
+}
