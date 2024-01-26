6425b7e2bd326dbf7bec55bbc22657d60627664b
stas_tarasevich <kuroe-ichi@yandex.ru>
2024-01-26 18:47:38 +0300

    Вторая задача

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int m = 2;
+int n = 3;
+Console.WriteLine($"A({m}, {n}) = {Ackermann(m, n)}");
+
+
+int Ackermann(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (n == 0)
+        return Ackermann(m - 1, 1);
+    else
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+}
